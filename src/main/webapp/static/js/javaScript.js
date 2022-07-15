f4637a03fac76da0b4d4a6ecf66e27bd523f456b
lilinkw <kjinh098@gmail.com>
2022-07-15 16:32:19 +0700

    Change display time when load new posts

--- src/main/webapp/static/js/javaScript.js
+++ src/main/webapp/static/js/javaScript.js
@@ -16,18 +16,37 @@
     function loadMorePost(){
         var postnum = document.getElementsByClassName("posty").length;
         $.ajax({
-            url: "/PetPeeves_war_exploded/LoadMoreServlet",
+            url: "LoadMoreServlet",
             type: "get", //send it through get method
             success: function(response) {
                 var a = document.getElementById("content");
-                console.log(a.innerHTML);
+                console.log(response);
                 a.innerHTML += response;
+
+                // Update post Time
+                let listPostTime = document.getElementsByClassName("postTime");;
+                for (var i = postnum; i < postnum+5; i++) {
+                    let diffTime = moment(listPostTime[i].text, "YYYY-MM-DD HH:m:s").fromNow();
+                    listPostTime[i].innerHTML = diffTime;
+                }
             },
             data: {
                 postShowed: postnum
             },
             error: function(xhr) {
                 //Do Something to handle error
-            }
+            },
+            dataType: "html",
         });
+
     }
+
+// $(document).ready(function(){
+//     $(window).on('scroll', function() {
+//         let reachedBottom = $(window).scrollTop() + $(window).height() > $(document).height() * 0.9;
+//
+//         if (reachedBottom) {
+//             loadMorePost();
+//         }
+//     });
+// });
